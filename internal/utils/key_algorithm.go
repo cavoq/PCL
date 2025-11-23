4868a479090f29f1baca519777834a2894049498
David Stromberger <cavoq@proton.me>
2025-11-23 19:44:11 +0100

    Refactor utils

--- internal/utils/key_algorithm.go
+++ internal/utils/key_algorithm.go
@@ -7,31 +7,6 @@ import (
 	"crypto/x509"
 )
 
-func GetSubjectNames(cert *x509.Certificate) []string {
-	var names []string
-
-	if cert.Subject.CommonName != "" {
-		names = append(names, cert.Subject.CommonName)
-	}
-
-	// Add SAN DNS names (important for leaf certs)
-	if len(cert.DNSNames) > 0 {
-		names = append(names, cert.DNSNames...)
-	}
-
-	return names
-}
-
-func GetIssuerNames(cert *x509.Certificate) []string {
-	var names []string
-
-	if cert.Issuer.CommonName != "" {
-		names = append(names, cert.Issuer.CommonName)
-	}
-
-	return names
-}
-
 type PublicKeyAlgorithm string
 
 const (
